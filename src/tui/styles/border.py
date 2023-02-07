62782b19011462f5a17ccc85703040044ebd6b62
Ivaylo Genchev <ivo.genchev4@gmail.com>
2023-02-07 05:58:10 +0200

    add border abstraction

---
+++ src/tui/styles/border.py
@@ -0,0 +1,27 @@
+"""Describe border frames"""
+
+from abc import ABC
+
+
+class Border(ABC):
+    """Abstract class for describing borders"""
+    vertical: str
+    horizontal: str
+    top_left: str
+    top_right: str
+    bottom_left: str
+    bottom_right: str
+
+
+class DefaultBorder(Border):
+    """Default border for components
+    ╔═╗
+    ║x║
+    ╚═╝
+    """
+    vertical = '║'
+    horizontal = '═'
+    top_left = '╔'
+    top_right = '╗'
+    bottom_right = '╝'
+    bottom_left = '╚'
